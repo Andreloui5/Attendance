876b4c7689719d869b104724c830670c1d6a330c
Craig Gant <craigkgant@gmail.com>
2020-02-20 12:00:58 -0500

    changed seeder

--- scripts/seed2.js
+++ scripts/seed2.js
@@ -5,6 +5,33 @@ const db = require("../models");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/attend");
 
+const eventSeed = [
+  {
+    keyword: "webhooktest",
+    host: "Orangewood Church",
+    type: "service",
+    date: "2016-06-12T10:00:00Z"
+  },
+  {
+    keyword: "webhooktest",
+    host: "Orangewood Church",
+    type: "service",
+    date: "2020-02-18T13:56:09-05:00"
+  },
+  {
+    keyword: "hello",
+    host: "Local Society",
+    type: "block party",
+    date: "2020-01-12T10:56:09-05:00"
+  },
+  {
+    keyword: "hello",
+    host: "Local Society",
+    type: "town hall meeting",
+    date: "2020-01-14T10:56:09-05:00"
+  }
+];
+
 const personSeed = [
   {
     mobile_number: "+12005551000",
@@ -24,6 +51,17 @@ const personSeed = [
   }
 ];
 
+db.Event.remove({})
+  .then(() => db.Event.collection.insertMany(eventSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
 db.Person.remove({})
   .then(() => db.Person.collection.insertMany(personSeed))
   .then(data => {
@@ -32,5 +70,5 @@ db.Person.remove({})
   })
   .catch(err => {
     console.error(err);
-    // process.exit(1);
+    process.exit(1);
   });
