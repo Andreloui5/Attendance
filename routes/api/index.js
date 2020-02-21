b8b46daa58d48e822ff59fd6445c3577e9584cd1
Craig Gant <craigkgant@gmail.com>
2020-02-21 11:25:06 -0500

    added definition to webhookRoutes in api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const peopleRoutes = require("./people");
 const eventsRoutes = require("./events");
+const webhookRoutes = require("./webhook");
 
 // tells express to use different routes
 router.use("/people", peopleRoutes);
